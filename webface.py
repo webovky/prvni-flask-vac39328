02c068ddfe48c30c53d81a80887d6118f7d61abc
Marrek Nožka <nozka@spseol.cz>
2022-11-10 12:12:37 +0100

    přihlášení

--- webface.py
+++ webface.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import functools
 
 # from werkzeug.security import generate_password_hash, check_password_hash
-
+ category
 app = Flask(__name__)
 app.secret_key = b"totoj e zceLa n@@@hodny retezec nejlep os.urandom(24)"
 app.secret_key = b"x6\x87j@\xd3\x88\x0e8\xe8pM\x13\r\xafa\x8b\xdbp\x8a\x1f\xd41\xb8"
 
 
-slova = ("Super", "Perfekt", "Úža", "Flask")
+slova = ("Super", "Perfekt", "Úža category", "Flask")
 
 
 def prihlasit(function):
@@ -37,8 +37,48 @@ def abc():
     return render_template("abc.html", slova=slova)
 
 
-
-
-@app.route("/malina/", methods=['GET', 'POST'])
+@app.route("/malina/", methods=["GET", "POST"])
 def malina():
-    return render_template('malina.html')
+    if "uživatel" not in session:
+        flash('Nejsi přihlášen. Tato stránka vyžaduje přihlášení.', 'error')
+        return redirect(url_for("login"))
+
+    hmotnost = request.args.get("hmotnost")
+    vyska = request.args.get("vyska")
+
+    print(hmotnost, vyska)
+    if hmotnost and vyska:
+        try:
+            hmotnost = float(hmotnost)
+            vyska = float(vyska)
+            bmi = hmotnost / (0.01 * vyska) ** 2
+        except (ZeroDivisionError, ValueError):
+            bmi = None
+            # err = 'Je třeba zadat dvě nenulová čísla!'
+    else:
+        bmi = None
+
+    return render_template("malina.html", bmi=bmi)
+
+
+@app.route("/login/", methods=["GET"])
+def login():
+    jmeno = request.args.get("jmeno")
+    heslo = request.args.get("heslo")
+    print(jmeno, heslo)
+    return render_template("login.html")
+
+
+@app.route("/login/", methods=["POST"])
+def login_post():
+    jmeno = request.form.get("jmeno")
+    heslo = request.form.get("heslo")
+    if jmeno == "marek" and heslo == "lokomotiva":
+        session["uživatel"] = jmeno
+    return redirect(url_for("login"))
+
+
+@app.route("/logout/", methods=["GET", "POST"])
+def logout():
+    session.pop("uživatel", None)
+    return redirect(url_for("index"))
