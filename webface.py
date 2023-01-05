9ae7f46f25c4f3a882cf27263823614193272223
Filip Vaca <vac39328@spseol.cz>
2023-01-05 14:14:26 +0100

    Zkracovač oprava

--- webface.py
+++ webface.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import functools
-
+import random
+import string
 # from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = b"totoj e zceLa n@@@hodny retezec nejlep os.urandom(24)"
@@ -38,6 +39,17 @@ def abc():
         return redirect(url_for("login",page=request.full_path))
     return render_template("abc.html", slova=slova)
 
+@app.route("/zkracovač/")
+def zkracovač():
+    return render_template("zkracovač.html")
+
+@app.route("/zkracovač/", methods = ["POST"])
+def zkracovač_post():
+    url = request.form.get("url")
+    zkratka = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    with SQLite("data1.db") as cur:
+                cur.execute("INSERT INTO user (zkratka, adresa) VALUES (?,?)", [zkratka, url])
+    return redirect(url_for("zkracovač"))
 
 @app.route("/malina/", methods=["GET", "POST"])
 def malina():
@@ -96,31 +108,27 @@ def registrace():
         jmeno = request.form.get("jmeno")
         heslo = request.form.get("heslo")
         heslo2 = request.form.get("heslo znovu")
-    if not (jmeno and heslo2 and heslo):
-        flash("Není vše vyplňeno!", "message")
-        return redirect(url_for("registrace"))
-    if heslo != heslo2:
-        flash("Hesla se neshodují!", "message")
-        return redirect(url_for("registrace"))
-    try:
-        with SQLite("data2.db") as cur:
-            cur.execute("INSERT INTO user (login, passwd) VALUES (?,?)", [jmeno, generate_password_hash(heslo)])
-            cur execute("SELECT passwd FROM user WHERE login = ?",[jmeno])
-            flash("Jsi registrován", "message")
-            flash("Jsi přihlášen", "message")
-            session["uživatel"] = jmeno
-            return redirect(url_for("index"))
-    except sqlite3.IntegrityError:
-        flash("Jmeno již existuje!", "message")
-        return redirect(url_for("registrace"))
+        if not (jmeno and heslo2 and heslo):
+            flash("Není vše vyplňeno!", "message")
+            return redirect(url_for("registrace"))
+        if heslo != heslo2:
+            flash("Hesla se neshodují!", "message")
+            return redirect(url_for("registrace"))
+        try:
+            with SQLite("data1.db") as cur:
+                cur.execute("INSERT INTO user (login, passwd) VALUES (?,?)", [jmeno, generate_password_hash(heslo)])
+                cur execute("SELECT passwd FROM user WHERE login = ?",[jmeno])
+                flash("Jsi registrován", "message")
+                flash("Jsi přihlášen", "message")
+                session["uživatel"] = jmeno
+                return redirect(url_for("index"))
+        except sqlite3.IntegrityError:
+            flash("Jmeno již existuje!", "message")
+            return redirect(url_for("registrace"))
 
-return redirect(url_for("registrace"))
+    return redirect(url_for("registrace"))
 """
 @app.route("/logout/", methods=["GET", "POST"])
 def logout():
     session.pop("uživatel", None)
     return redirect(url_for("index"))
-
-@app.route("/zkracovač/")
-def zkracovač():
-    return render_template("zkracovač.html")
